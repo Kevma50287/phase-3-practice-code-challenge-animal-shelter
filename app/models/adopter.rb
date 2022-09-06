ee7ab8520058fe42a4e0697989ce026ff3e803db
Kevin <magavakevin10@gmail.com>
2022-09-06 13:37:57 -0400

    Done

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -1,4 +1,28 @@
 class Adopter < ActiveRecord::Base
     has_many :pets
     has_many :shelters, through: :pets
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def adopt(pet)
+        pet[:adopted?] = true
+        pet[:adopter_id] = self.id
+        pet.save
+    end
+
+    def fav_pet
+        if self.pets.count == 0
+            nil
+        else
+            species_array = self.pets.pluck(:species)
+            species_array.max_by{|specie| species_array.count(specie)}
+
+            # self.pets
+            # .group_by { |pet| pet.species}
+            # .max_by { |species| species.count }
+            # [0]
+        end
+    end
 end
